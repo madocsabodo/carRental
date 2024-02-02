82fa31163b3015554fd56a30010814c182e70f2e
madocsabodo <56889441+kati1989@users.noreply.github.com>
2024-02-03 00:36:46 +0200

    comment

--- src/main/java/edu/datafusion/CarRental/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/edu/datafusion/CarRental/AuthenticationSuccessHandlerImpl.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ *
+ */
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
